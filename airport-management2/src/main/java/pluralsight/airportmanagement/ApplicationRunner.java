7a7f0e550ca26fde03bb2ae23fb6f59f63403cb1
rafaelacirino <rafaelaborbaf@gmail.com>
2021-12-27 10:48:35 -0300

    Module 4

--- airport-management2/src/main/java/pluralsight/airportmanagement/ApplicationRunner.java
+++ airport-management2/src/main/java/pluralsight/airportmanagement/ApplicationRunner.java
@@ -1,9 +1,12 @@
 package pluralsight.airportmanagement;
 
+import java.util.List;
+
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Component;
 import pluralsight.airportmanagement.domain.FlightInformation;
+import pluralsight.airportmanagement.domain.FlightPrinter;
+import pluralsight.airportmanagement.queries.FlightInformationQueries;
 
 /*
 In Spring Boot, a class that implements CommandLineRunner
@@ -13,19 +16,23 @@ is executed after the application is bootstrapped
 @Component
 public class ApplicationRunner implements CommandLineRunner {
 
-    private MongoTemplate mongoTemplate;
+    private FlightInformationQueries flightInformationQueries;
 
-    public ApplicationRunner(MongoTemplate mongoTemplate) {
-        this.mongoTemplate = mongoTemplate;
+    public ApplicationRunner(FlightInformationQueries flightInformationQueries) {
+        this.flightInformationQueries = flightInformationQueries;
     }
 
     @Override
-    public void run(String... args) throws Exception {
-
-        FlightInformation emptyFlight = new FlightInformation();
-        this.mongoTemplate.save(emptyFlight);
-
-        System.out.println("Application started...");
+    public void run(String... args) {
+    	System.out.print("-----\nQuery: All flights ordered by departure");
+    	List<FlightInformation> allFlightsOrdered = this.flightInformationQueries
+    			.findAll("departure", 0, 10);
+    	FlightPrinter.print(allFlightsOrdered);
+    	
+    	System.out.print("-----\nQuery: Free text search: Rome");
+    	List<FlightInformation> flightsByFreeText = this.flightInformationQueries
+    			.findByFreeText("Rome");
+    	FlightPrinter.print(flightsByFreeText);     
     }
 }
 
--- airport-management2/src/main/java/pluralsight/airportmanagement/domain/FlightInformation.java
+++ airport-management2/src/main/java/pluralsight/airportmanagement/domain/FlightInformation.java
@@ -3,23 +3,31 @@ package pluralsight.airportmanagement.domain;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.Transient;
 import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.index.TextIndexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
 import java.time.LocalDate;
+import java.util.UUID;
 
 @Document("flights")
 public class FlightInformation {
     @Id
     private String id;
 
+    @Indexed(unique = true)
+    private String internalId;
+
     @Field("departure")
-    @Indexed
+    @TextIndexed
     private String departureCity;
 
     @Field("destination")
-    @Indexed
+    @TextIndexed
     private String destinationCity;
+
+    @TextIndexed(weight = 2)
+    private String description;
 
     private FlightType type;
     private boolean isDelayed;
@@ -30,8 +38,9 @@ public class FlightInformation {
     @Transient
     private LocalDate createdAt;
 
-    public FlightInformation(){
+    public FlightInformation() {
         this.createdAt = LocalDate.now();
+        this.internalId = UUID.randomUUID().toString();
     }
 
     public String getId() {
@@ -96,5 +105,17 @@ public class FlightInformation {
 
     public LocalDate getCreatedAt() {
         return createdAt;
+    }
+
+    public String getInternalId() {
+        return internalId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
